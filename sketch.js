33a2ba3bd482b3cf918a9d829d36dd0a7cfcc861
aaron <aaron.yeo129@gmail.com>
2021-06-25 09:07:46 +0800

    init

---
+++ sketch.js
@@ -0,0 +1,60 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+
+  // put setup code here
+}
+
+
+function draw() {
+  fill(0, 170, 255)
+  rect(20, 20, 500, 600)
+  textSize(45)
+  let a = 'CHEAP WATER'
+  fill(10)
+  text(a,180,50,100,100)
+
+  fill(0, 170, 255)
+  rect(20, 20, 400, 500
+
+  textSize(16)
+  let b = 'ℓ = 1¢     Buy 3ℓ get 1ℓ free'
+  fill(10)
+  text(b,350,290,100,100)
+
+  fill(230,251,255)
+  rect(70, 300, 200, 50)
+
+  fill(230,251,255)
+  rect(70, 390, 200, 50)
+
+  fill(230,251,255)
+  rect(70, 480, 200, 50)
+
+  textSize(15)
+  let c = 'Choose your language'
+  fill(10)
+  text(c,70,260,100,100)
+
+  textSize(15)
+  let d = 'Choose your reigon'
+  fill(10)
+  text(d,70,350,100,100)
+
+  textSize(15)
+  let e = 'Choose your country'
+  fill(10)
+  text(e,70,440,100,100)
+
+  fill(50,205,50)
+  ellipse(400,450,200,90)
+
+  textSize(60)
+  let f = 'PAY'
+  fill(10)
+  text(f,370,400,100,100)
+
+
+  fill(0, 170, 255)
+  rect(20, 20, 1000, 2000)
+}
+draw()
